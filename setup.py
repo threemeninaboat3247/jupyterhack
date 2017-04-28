2815488a60528102eaeaac8422d6de6d37699e7a
threemeninaboat3247 <threemeninaboat3247@gmail.com>
2017-04-29 01:02:53 +0900

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+
+try:
+    import setuptools
+    from setuptools import setup, find_packages
+except ImportError:
+    print("Please install setuptools.")
+
+import os
+long_description = 'a data browser which has a tree structure'
+
+setup(
+    name  = 'JupyterHuck',
+    version = '0.0',
+    description = 'Visualizing data in a tree structure',
+    long_description = long_description,
+    license = 'MIT',
+    author = 'Yuki Arai',
+    author_email = 'threemeninaboat3247@gmail.com',
+    url = 'https://github.com/threemeninaboat3247/JupyterHuck',
+    keywords = 'data browser',
+    packages = find_packages(),
+    install_requires = ['PyQt5', 'numpy','scipy','matplotlib','pandas'],
+    classifiers = [
+      'Programming Language :: Python :: 3.5',
+      'Intended Audience :: Science/Research',
+      'License :: OSI Approved :: MIT License'
+    ]
+)
